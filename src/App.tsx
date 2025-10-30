e2ca47a75fab3ce202741d6ca9c0d71b7704bfaf
jnajajreh <jnajajreh@bethlehem.edu>
2025-10-30 13:51:14 +0200

    Initional commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState, type JSX } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [number, setNumber] = useState(0);
+
+  async function getAdvice() {
+    const resp = await fetch("https://api.adviceslip.com/advice");
+    const data = await resp.json();
+    setAdvice(data.slip.advice);
+    // setNumber(number+1);
+    setNumber((prevNumber) => prevNumber + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice} </h1>
+      <button onClick={getAdvice}>Click Me!</button>
+      <textarea name="name"> </textarea>
+      <Message number={number} country="Jordan" />
+    </div>
+  );
+}
+
+interface MessageProps {
+  number: number;
+  country: string;
+  c?: string;
+}
+
+function Message(props: MessageProps): JSX.Element {
+  return (
+    <div>
+      <h2>Advice Count: {props.number} </h2>
+      <p>Country : {props.country}</p>
+      <p>Country : {props.c}</p>
+    </div>
+  );
+}
